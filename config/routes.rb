597967206518a694b2175410fa29af1571134ca0
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-09-06 08:31:07 +0000

    [finish]ouyou2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :index, :show, :destroy]
+  resources :post_images
   devise_for :users
   root to: "homes#top"
   get "homes/about", as: 'about'
