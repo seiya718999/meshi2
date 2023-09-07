70fbfffa878b501cbaf09105cfa4db18f87021ba
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-09-07 14:13:01 +0000

    [finish]ouyou2

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,13 +7,15 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to_ post_image_path
+    redirect_to post_images_path
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:])
   end
   
   private
