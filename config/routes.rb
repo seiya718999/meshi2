dd0dacb920641eeed41744ccf6a4a84eb344391f
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-09-11 12:54:17 +0000

    [finish]ouyou2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   resources :post_images
+    resources :post_comments, only: [:create] 
   devise_for :users
   root to: "homes#top"
   get "homes/about", as: 'about'
