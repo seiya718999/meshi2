fcb42f2d73ee8becbff2fbd07ec7e565961c1d46
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-09-11 12:22:52 +0000

    [finish]ouyou2 git commit -m [finish]ouyou2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit]
+  
   resources :post_images
   devise_for :users
   root to: "homes#top"
   get "homes/about", as: 'about'
+  resources :users, only: [:show, :edit]
 end
+
