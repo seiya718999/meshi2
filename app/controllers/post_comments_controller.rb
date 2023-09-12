01ac6e5d2c0c22189ae41703cce0801e4e563c6a
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-09-12 01:07:57 +0000

    [finish]ouyou2

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,17 +1,18 @@
-class PostCommentController < ApplicationController
-  
+class PostCommentsController < ApplicationController
+
+
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.post_image.id = post_image.id
+    comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image.id)
   end
-  
+
   private
-  
+
   def post_comment_params
     params.require(:post_comment).permit(:commen)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
-  resources :post_images do
-    resources :post_comments, only: [:create] 
-  end
   devise_for :users
   root to: "homes#top"
   get "homes/about", as: 'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create]
+  end
   resources :users, only: [:show, :edit, :update]
 end
 
